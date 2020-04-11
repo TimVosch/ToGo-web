0b10ebad486ad9078e9487ff72940cfe95890e88
Tim van Osch <timvosch@pollex.nl>
2020-04-11 15:57:07 +0200

    Move navigation into folder

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,4 @@
-import { FunctionalComponent } from "preact";
-import { Navigation } from "../components/navigation";
+import { Navigation } from "../components/navigation/navigation";
 
 export const HomePage: FunctionalComponent = () => (
   <div>
