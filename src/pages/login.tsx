b65fb29b3b68370793053259d91d1e7200353f50
Tim van Osch <timvosch@pollex.nl>
2020-04-11 21:46:14 +0200

    Add login redirection

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,17 +1,31 @@
 import { h, Component, ComponentChild, Fragment } from "preact";
+import { route } from "preact-router";
 import { Navigation } from "../components/navigation";
 import { LoginForm } from "../components/login-form";
 import { LoginService } from "../services/login/login.service";
+import { AuthenticationError } from "../services/login/authentication.error";
 
 export class LoginPage extends Component {
   private readonly loginService = new LoginService();
 
+  componentDidMount(): void {
+    if (this.loginService.isLoggedIn()) {
+      // TODO: Modal
+      alert("Already logged in!");
+      route("/");
+    }
+  }
+
   async login(email: string, password: string): Promise<void> {
     try {
+      // Login and redirect
       await this.loginService.login(email, password);
+      route("/");
     } catch (e) {
-      // eslint-disable-next-line
-      console.error(e);
+      if (e instanceof AuthenticationError) {
+        alert(e);
+      }
+      // TODO: we dont know what happened.
     }
   }
 
