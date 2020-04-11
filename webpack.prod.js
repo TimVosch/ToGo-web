2202fbe177e547746ddb808a0a4e9b5f1e8453af
Tim van Osch <timvosch@pollex.nl>
2020-04-11 15:18:02 +0200

    Add file-loader

--- webpack.prod.js
+++ webpack.prod.js
@@ -34,6 +34,17 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "images/[hash]-[name].[ext]",
+            },
+          },
+        ],
+      },
     ],
   },
   resolve: {
