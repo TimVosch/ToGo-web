5740d35e1f15c2adddb3d9b60663d631af7d1a19
Tim van Osch <timvosch@pollex.nl>
2020-04-11 17:42:29 +0200

    Move input-card to component

---
+++ src/components/input-card/input-card.tsx
@@ -0,0 +1,25 @@
+import { h, FunctionComponent } from "preact";
+
+interface InputCardPropTypes {
+  value: string;
+  placeholder: string;
+  onSubmit: () => void;
+}
+
+export const InputCard: FunctionComponent<InputCardPropTypes> = (props) => (
+  <div class="flex w-1/2 mx-auto items-center mt-5 shadow-lg border rounded-lg">
+    <input
+      type="text"
+      name="todo"
+      id="todo-input"
+      placeholder={props.placeholder || "Enter new todo..."}
+      class="focus:outline-none flex-1 p-3 text-md rounded-l-lg"
+    />
+    <button
+      className="h-100 py-3 px-5 text-white font-semibold bg-green-400 hover:bg-green-500 rounded-r-lg transition-background duration-100"
+      onClick={props.onSubmit}
+    >
+      {props.value || "Submit"}
+    </button>
+  </div>
+);
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,51 @@
+import { h, Component, ComponentChild } from "preact";
 import { Navigation } from "../components/navigation/navigation";
+import { InputCard } from "../components/input-card/input-card";
+import { TodoService } from "../services/todo/todo.service";
 
-export const HomePage: FunctionalComponent = () => (
-  <div>
-    <Navigation />
-  </div>
-);
+export class HomePage extends Component {
+  todos = new TodoService();
+
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  addTodo(): void {
+    alert(this.constructor.name);
+  }
+
+  genList(): ComponentChild[] {
+    const items = this.todos.getTodos().map((todo) => (
+      <div
+        key={todo.id}
+        class="flex w-1/2 mx-auto mt-5 shadow-md rounded-lg border-t border-gray-400"
+      >
+        <input
+          type="checkbox"
+          name="completed"
+          id="todo-completed"
+          class="ml-3"
+        />
+        <div class="p-3 text-md">{todo.title}</div>
+      </div>
+    ));
+
+    return items;
+  }
+
+  render(): ComponentChild {
+    const list = this.genList();
+    return (
+      <div>
+        <Navigation />
+        <InputCard
+          placeholder="Add new todo..."
+          value="Add"
+          onSubmit={this.addTodo.bind(this)}
+        />
+        {list}
+      </div>
+    );
+  }
+}
